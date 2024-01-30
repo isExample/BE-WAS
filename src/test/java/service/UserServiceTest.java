13046e4b6e91de20072969c68a593558c85ff6d7
isexample <yeji0331@naver.com>
2024-01-30 11:48:19 +0900

    [Refactor] Test 관련 디렉토리명 수정
    Service 계층의 테스트 코드를 하나의 디렉토리에서 관리하고자  디렉토리명을 수정했습니다.
    
    - user -> service

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,11 +1,10 @@
-package user;
+package service;
 
 import db.Database;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import service.UserService;
 
 import java.util.HashMap;
 import java.util.Map;
