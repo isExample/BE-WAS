6a9d297a8af7c40a0196b882c2b4fb4ce29560e9
isexample <yeji0331@naver.com>
2024-01-30 11:19:19 +0900

    [Test] UserServiceTest의 회원가입 관련 테스트 코드 개선
    회원가입 기능에 대한 테스트 코드를 리팩토링하여 가독성과 직관성을 향상시켰습니다.
    
    - @DisplayName 어노테이션을 추가
    - 테스트 구조 Given-When-Then 형식으로 표현

--- src/test/java/user/UserServiceTest.java
+++ src/test/java/user/UserServiceTest.java
@@ -3,6 +3,7 @@ package user;
 import db.Database;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import service.UserService;
 
@@ -27,43 +28,59 @@ public class UserServiceTest {
     }
 
     @Test
-    public void signUpSuccessTest() {
-        Map<String, String> queryParams = new HashMap<>();
-        queryParams.put("userId", "isExample");
-        queryParams.put("password", "password123");
-        queryParams.put("name", "isExample");
-        queryParams.put("email", "isExample@naver.com");
+    @DisplayName("유효한 사용자 데이터가 주어지면 가입 시 true를 반환한다")
+    public void signUpSuccess() {
+        // Given
+        Map<String, String> validUserData = new HashMap<>();
+        validUserData.put("userId", "isExample");
+        validUserData.put("password", "password123");
+        validUserData.put("name", "isExample");
+        validUserData.put("email", "isExample@naver.com");
 
-        assertTrue(userService.signUp(queryParams));
+        // When
+        boolean result = userService.signUp(validUserData);
+
+        // Then
+        assertTrue(result);
     }
 
     @Test
-    public void signUpFailureDueToDuplicateUserIdTest() {
-        // isExample 사용자를 하나 추가
-        userService.signUp(new HashMap<>() {{
-            put("userId", "isExample");
-            put("password", "password123");
-            put("name", "isExample");
-            put("email", "isExample@naver.com");
-        }});
+    @DisplayName("유효한 사용자 데이터라도 userId가 중복되면 false를 반환한다")
+    public void signUpFailureDuplicateUserId() {
+        // Given
+        Map<String, String> validUserData1 = new HashMap<>();
+        validUserData1.put("userId", "isExample");
+        validUserData1.put("password", "password123");
+        validUserData1.put("name", "isExample");
+        validUserData1.put("email", "isExample@naver.com");
 
-        // 동일한 userId로 다시 시도
-        assertFalse(userService.signUp(new HashMap<>() {{
-            put("userId", "isExample");
-            put("password", "newPassword");
-            put("name", "user2");
-            put("email", "user2@gmail.com");
-        }}));
+        Map<String, String> validUserData2 = new HashMap<>();
+        validUserData2.put("userId", "isExample");
+        validUserData2.put("password", "testing13");
+        validUserData2.put("name", "testUser");
+        validUserData2.put("email", "testUser@test.com");
+
+        // When
+        userService.signUp(validUserData1);
+        boolean result = userService.signUp(validUserData2);
+
+        // Then
+        assertFalse(result);
     }
 
     @Test
-    public void signUpFailureDueToMissingParamsTest() {
-        // name과 email이 누락됨
-        Map<String, String> queryParams = new HashMap<>();
-        queryParams.put("userId", "user");
-        queryParams.put("password", "password123");
+    @DisplayName("회원가입 폼의 파라미터값이 누락되면 false를 반환한다")
+    public void signUpFailureMissingParams() {
+        // Given: name과 email 누락
+        Map<String, String> bodyParams = new HashMap<>();
+        bodyParams.put("userId", "user");
+        bodyParams.put("password", "password123");
 
-        assertFalse(userService.signUp(queryParams));
+        // When
+        boolean result = userService.signUp(bodyParams);
+
+        // Then
+        assertFalse(result);
     }
 
 }
